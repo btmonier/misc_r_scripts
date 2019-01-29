9e0c8b7b3930eb8885aff47cee84bc704bf4025e
Brandon Monier <brandon.monier@gmail.com>
2019-01-28 22:46:41 -0500

    Add new colors

--- croix.R
+++ croix.R
@@ -3,11 +3,11 @@
 croix_palettes <- list(
 	pure = c(
 		"#B5E0EC", "#98C6D5", "#5DC5DC", "#43A2BA",
-		"#0E266B", "#80A3B2", "#0991C4", "#CBC8C9"	
+		"#0E266B", "#80A3B2", "#0991C4", "#CBC8C9"
 	),
 	berry = c(
 		"#C691B6", "#B37CA3", "#D4AAC7", "#E5C0D8",
-		"#A1648E", "#172664", "#1784AD", "#65689C"	
+		"#A1648E", "#172664", "#1784AD", "#65689C"
 	),
 	cran_raspberry = c(
 		"#E48887", "#EDA8A6", "#C56869", "#BC8586",
@@ -15,54 +15,158 @@ croix_palettes <- list(
 	),
 	lemon = c(
 		"#F0E086", "#CBB864", "#EFC805", "#CEA407",
-		"#142265", "#80875C", "#1689B0", "#BFBFBB"	
+		"#142265", "#80875C", "#1689B0", "#BFBFBB"
 	),
 	lime = c(
 		"#9CC26D", "#7FAA4E", "#B7D68F", "#0C1F69",
-		"#D2DEB9", "#209E13", "#088BB3", "#52857A"	
+		"#D2DEB9", "#209E13", "#088BB3", "#52857A"
 	),
 	orange = c(
 		"#F2B855", "#EE9916", "#C97912", "#CB973E",
-		"#16265E", "#767250", "#128992", "#C9C3BE"	
+		"#16265E", "#767250", "#128992", "#C9C3BE"
 	),
 	pamplemousse = c(
 		"#EFC38E", "#CA9C68", "#D8848A", "#F3A5A8",
-		"#1F3172", "#837985", "#2293AF", "#C1BCBC"	
+		"#1F3172", "#837985", "#2293AF", "#C1BCBC"
 	),
 	peach_pear = c(
 		"#DABC86", "#E6E3A5", "#DC9067", "#9F9674",
-		"#1F3070", "#2695AD", "#E54613", "#BFC4BE"	
+		"#1F3070", "#2695AD", "#E54613", "#BFC4BE"
 	),
 	coconut = c(
 		"#E5E4E6", "#E0CFBE", "#CDB296", "#19296E",
-		"#B0B7BC", "#2299BB", "#978580", "#96461E"	
+		"#B0B7BC", "#2299BB", "#978580", "#96461E"
 	),
 	apricot = c(
 		"#EC9E0A", "#E4E0E1", "#D06D0F", "#C1BCBF",
-		"#151F59", "#7F868E", "#E6A26D", "#20A4A4"	
+		"#151F59", "#7F868E", "#E6A26D", "#20A4A4"
 	),
 	passionfruit = c(
 		"#EC6883", "#C84163", "#E7E3E8", "#CEC3C9",
-		"#15215E", "#507F9B", "#AF99AA", "#AEBF1A"	
+		"#15215E", "#507F9B", "#AF99AA", "#AEBF1A"
 	),
 	key_lime = c(
 		"#0B8733", "#91D30C", "#65AB10", "#11205D",
-		"#CBD8BF", "#8CBF70", "#ECED05", "#BE5416"	
+		"#CBD8BF", "#8CBF70", "#ECED05", "#BE5416"
 	),
 	mango = c(
 		"#86BF0C", "#E8E8E9", "#121E5A", "#CECECD",
-		"#379A53", "#B3B8A6", "#7F8B96", "#E64313"	
+		"#379A53", "#B3B8A6", "#7F8B96", "#E64313"
 	),
 	tangerine = c(
 		"#ECA024", "#F3B854", "#D5701F", "#101D5D",
-		"#4F4A61", "#978955", "#D0C9C3", "#28ACA0"	
+		"#4F4A61", "#978955", "#D0C9C3", "#28ACA0"
+	),
+	the_shining = c(
+	    "#381D12", "#F89787", "#3C5D37", "#698A91", "#A33D15",
+	    "#564531", "#2D3743", "#A06F5E", "#5D0C08", "#0D0A03"
+	),
+	the_big_lebowski = c(
+	    "#6A6D76", "#71270E", "#272729", "#755347", "#9FA5E3",
+	    "#978E7F", "#455A5B", "#663E0D", "#825167", "#39394D"
+	),
+	star_wars_ep_v = c(
+	    "#3B241E", "#495B83", "#2A2432", "#F46249", "#87758D",
+	    "#849DB3", "#4D331A", "#E5BFA8", "#97AD96", "#1A1E3B"
+	),
+	the_godfather = c(
+	    "#1F191B", "#502A17", "#854C16", "#1C1B17", "#CFBF9D",
+	    "#BF2316", "#F0E7BC", "#EFAC58", "#B97F1B", "#4B3C1D"
+	),
+	the_revenant = c(
+	    "#161B1E", "#262E31", "#7F7C77", "#E8DFE0", "#B0B9C2",
+	    "#92AFC0", "#4E8BA8", "#103E55", "#0F3F54", "#020E17"
+	),
+	fargo = c(
+	    "#41494B", "#190A07", "#52382B", "#3C1414", "#FFBCCF",
+	    "#AA7B75", "#47423C", "#5C0806", "#E1ECFE", "#FE7A88"
+	),
+	the_professional = c(
+	    "#665E47", "#9D8B76", "#B4775B", "#AD3429", "#6B140B",
+	    "#B65D35", "#C19029", "#464E29", "#3D4753", "#040001"
+	),
+	akira = c(
+	    "#115363", "#9D3011", "#5C4356", "#7EA030", "#7E2835",
+	    "#756B2E", "#02182E", "#9E0402", "#1A3330", "#1F2549"
+	),
+	pulp_fiction = c(
+	    "#060408", "#BA8B5F", "#EBDAC0", "#710906", "#241206",
+	    "#4A403F", "#3E5BFF", "#280203", "#161908", "#343A36"
+	),
+	taxi_driver = c(
+	    "#271B1D", "#ABA5A7", "#5D474A", "#443A32", "#735D34",
+	    "#784542", "#857983", "#230D10", "#AB9392", "#5B6042"
+	),
+	a_clockwork_orange = c(
+	    "#D8C406", "#C28204", "#F95F33", "#DF2318", "#45161C",
+	    "#1D131B", "#23284E", "#2557D0", "#233C26", "#1A1C17"
+	),
+	ferris_buellers_day_off = c(
+	    "#775B4F", "#F7C7B9", "#D5D9FC", "#2F3744", "#8695AA",
+	    "#C4C3C8", "#4A5A67", "#0D0B10", "#817775", "#241A19"
+	),
+	fear_and_loathing_in_las_vegas = c(
+	    "#6F3F19", "#A68166", "#D2382E", "#FFDCC9", "#D79460",
+	    "#FFBE46", "#903F00", "#2F130F", "#5A5865", "#3E350C"
+	),
+	american_beauty = c(
+	    "#E2A9B2", "#AF6B84", "#77404C", "#4D222B", "#341211",
+	    "#2B0105", "#490102", "#7B0400", "#860315", "#B8133D"
+	),
+	blade_runner = c(
+	    "#6D3F42", "#946C86", "#654F7E", "#3C2E48", "#182132",
+	    "#223F6B", "#547FA2", "#94B4B3", "#69C0B8", "#216B64"
+	),
+	alien = c(
+	    "#0B3242", "#0A1A1A", "#000000", "#232E30", "#445766",
+	    "#AFBACC", "#D4D9DC", "#B7A29F", "#764B55", "#362722"
+	),
+	napoleon_dynamite = c(
+	    "#3E0D10", "#94635E", "#58619C", "#C19733", "#3E2123",
+	    "#9D3303", "#D1CDDC", "#4D3F3F", "#7C434A", "#0F040A"
+	),
+	edward_scissorhands = c(
+	    "#5D709B", "#B3897B", "#5F5D22", "#7C554E", "#F59886",
+	    "#E0C1B6", "#C67DB4", "#A9A96B", "#9692A0", "#F46E4B"
+	),
+	thelma_and_louise = c(
+	    "#333436", "#B57A9E", "#7C585A", "#9894BA", "#68151D",
+	    "#472C17", "#6D695E", "#CA927B", "#8898B1", "#5E7856"
+	),
+	fight_club = c(
+	    "#292B15", "#99D4E6", "#08171C", "#5B1714", "#A69B6E",
+	    "#F2BE37", "#C0B9E5", "#E0E0E2", "#30DDA7", "#010207"
+	),
+	the_truman_show = c(
+	    "#0E0B12", "#262431", "#575E70", "#A6A6B0", "#D7D1DB",
+	    "#BAC1D3", "#A0BAD5", "#859ED2", "#5477AD", "#142F61"
+	),
+	mad_max_fury_road = c(
+	    "#0D646B", "#16383A", "#414640", "#B88F73", "#FFD2B5",
+	    "#ED8F49", "#E27812", "#5C2A09", "#2E1612", "#0F1217"
+	),
+	resevoir_dogs = c(
+	    "#513F4F", "#4E1B0A", "#2C191D", "#9B5764", "#4B577D",
+	    "#5C373E", "#62090D", "#D8BCE2", "#19080E", "#A67D83"
+	),
+	blue_velvet = c(
+	    "#000103", "#120807", "#1E0606", "#3F0C11", "#5A050C",
+	    "#79022A", "#971E54", "#E15C9E", "#FDA9DD", "#F3BCC9"
+	),
+	silence_of_the_lambs = c(
+	    "#726554", "#371A16", "#A45D4A", "#C6BEBB", "#9A1E00",
+	    "#120D09", "#613613", "#80371E", "#DCB8A2", "#4C0D00"
+	),
+	the_matrix = c(
+	    "#060407", "#0B1013", "#841211", "#1E2E2D", "#344745",
+	    "#60807B", "#7D9A88", "#9DBEB3", "#C1D6C7", "#DCEEF2"
 	)
 )
 
 
 croix_palette <- function(name, n = NULL, type = c("discrete", "continuous")) {
 	type <- match.arg(type)
-	
+
 	pal <- croix_palettes[[name]]
 	if (is.null(pal)) {
 		stop("This palette could not be found.")
@@ -73,9 +177,9 @@ croix_palette <- function(name, n = NULL, type = c("discrete", "continuous")) {
 	} else if (type == "discrete" && n > length(pal)) {
 		warning(
 			paste0(
-				"n too large, allowed maximum for palette ", 
+				"n too large, allowed maximum for palette ",
 				"\"", name, "\"", " is ", length(pal), ".\n",
-				"Displaying the palette you asked for with that many colors.",
+				"  Displaying the palette you asked for with that many colors.",
 				"\n"
 			)
 		)
@@ -100,7 +204,7 @@ syntax <- function(x) {
   	x <- paste(
   		toupper(
   			substring(x, 1, 1)
-  		), 
+  		),
   		substring(x, 2),
       	sep = "", collapse = " "
   	)
@@ -108,20 +212,20 @@ syntax <- function(x) {
 }
 
 
-print.croix <- function(x, ...) {
+plot.croix <- function(x, ...) {
 	n <- length(x)
 	old <- par(pty = "m", mar = c(3, 1, 3, 1))
 	on.exit(par(old))
 
 	image(
-		x = 0:n, 
-		y = c(0, 2.5), 
-		z = as.matrix(1:n), 
+		x = seq(from = 0, to = n),
+		y = c(0, 2.5),
+		z = as.matrix(1:n),
 		col = x,
 		ylab = "",
-		xlab = "", 
-		xaxt = "n", 
-		yaxt = "n", 
+		xlab = "",
+		xaxt = "n",
+		yaxt = "n",
 		bty = "n"
 	)
 
@@ -129,22 +233,22 @@ print.croix <- function(x, ...) {
 	b_lab <- paste("Colors:", n)
 
 	rect(
-		xleft = 0, 
-		ybottom = 0, 
-		xright = n, 
-		ytop = 0.5, 
-		col = rgb(1, 1, 1), 
+		xleft = 0,
+		ybottom = 0,
+		xright = n,
+		ytop = 0.5,
+		col = rgb(1, 1, 1),
 		border = NA
 	)
 
 	rect(
-		xleft = 0, 
-		ybottom = 2, 
-		xright = n, 
-		ytop = 2.5, 
-		col = rgb(1, 1, 1), 
+		xleft = 0,
+		ybottom = 2,
+		xright = n,
+		ytop = 2.5,
+		col = rgb(1, 1, 1),
 		border = NA
-	)	
+	)
 	text(n * 0.5, 2.25, labels = t_lab, cex = 1, font = 2)
 	text(n * 0.5, 0.25, labels = b_lab, cex = 1, font = 1)
 }
@@ -157,15 +261,17 @@ summary.croix <- function(x, ...) {
 	cat("  Type......", attr(x, "type"), "\n")
 }
 
-
+print.croix <- function(x, ...) {
+    print(as.vector(x))
+}
 
 
 
 
 croix_all <- function(...) {
 	names <- names(croix_palettes)
-	max_len <- 8
-	par(mar = c(1, 1, 2, 1), pty = "s")
+	max_len <- max(lengths(croix_palettes))
+	par(mar = c(0.5, 12, 2, 1), pty = "s")
 
 	plot(
 		x = c(0, max_len),
@@ -180,23 +286,23 @@ croix_all <- function(...) {
 	for (i in seq_len(max_len)) {
 		for (j in seq_along(names)) {
 			rect(
-				xleft = i - 1, 
-				ybottom = j - 1, 
-				xright = i, 
-				ytop = j - 0.2, 
-				col = croix_palette(names[j])[i], 
+				xleft = i - 1,
+				ybottom = j - 1,
+				xright = i,
+				ytop = j - 0.2,
+				col = croix_palette(names[j])[i],
 				border = "white",
 				lwd = 5
-			)	
+			)
 		}
 	}
 
 	text(
-		rep(-0.1, length(names)), 
-		seq_along(names) - 0.6, 
-		labels = names, 
-		xpd = TRUE, 
+		rep(-0.1, length(names)),
+		seq_along(names) - 0.6,
+		labels = names,
+		xpd = TRUE,
 		adj = 1
-	)	
+	)
 }
 
