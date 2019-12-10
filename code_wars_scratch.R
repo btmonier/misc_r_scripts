942959a3dbab36ca676942d7eae708e56f6619f4
Brandon <brandon.monier@gmail.com>
2019-12-10 16:27:15 -0500

    Add new challenge

--- code_wars_scratch.R
+++ code_wars_scratch.R
@@ -1,4 +1,4 @@
-# === CW 001 ===+
+# === CW 001 ========================================================
 
 library(testthat)
 
@@ -19,8 +19,10 @@ testthat::test_that("Sample Tests", {
 
 
 
-# === CW 002 ====
+# === CW 002 ========================================================
 bouncingBall <- function(h, bounce, window) {
+    
+    ## Logic
     if (h <= 0) {
         return(-1)
     }
@@ -33,29 +35,34 @@ bouncingBall <- function(h, bounce, window) {
         return(-1)
     }
     
-    
-    bounce_data <- list()
-    i <- TRUE
-    while (i) {
-        new_height <- bounce * h
-        if (new_height <= window) {
-            new_height <- bounce * new_height
-            message("bounce at ", new_height, " meters...")
-            i <- TRUE
-        } else {
-            message("stop!")
-            i <- F
-        }
-        # message("bounce...")
+    ## Iterate through variables while new height > window height
+    new_height <- h * bounce
+    fall_data <- 1 # will always fall at start...
+    bounce_data <- 0
+    while (new_height > window) {
+        new_height <- bounce * new_height
+        fall_data <- fall_data + 1
+        bounce_data <- bounce_data + 1
     }
-    return(2)
+    return(fall_data + bounce_data)
 }
 
 
 
+# === CW 003 ========================================================
 
-
-
+dig_power <- function(n, p) {
+    if (n < 0 || p < 0) return(-1)
+    
+    num_string <- as.numeric(strsplit(as.character(n), "")[[1]])
+    
+    powers <- seq_len(num_string) + (p - 1)
+    
+    form_iter <- num_string ^ powers
+    
+    sum_iter <- sum(form_iter)
+    return(num_string)
+}
 
 
 
