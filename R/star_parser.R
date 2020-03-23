6514864e1c164bd646ff3c0eaff71bcd82799938
Brandon <brandon.monier@gmail.com>
2020-03-23 15:09:10 -0400

    Script cleaning

--- R/star_parser.R
+++ R/star_parser.R
@@ -5,7 +5,7 @@
 # Description:   Parse STAR alignment statistics files
 # Author:        Brandon Monier
 # Created:       2020-03-06 at 13:39:29
-# Last Modified: 2020-03-06 at 17:29:55
+# Last Modified: 2020-03-20 at 12:29:55
 #--------------------------------------------------------------------
 
 #--------------------------------------------------------------------
@@ -24,8 +24,11 @@ library(tibble)
 
 
 ## Parameters ----
-path <- "~/Temporary/sorghum_rna_stats/star_stats"
-pattern <- "/home/btmonier/Temporary/sorghum_rna_stats/star_stats/"
+# path <- "~/Temporary/sorghum_rna_stats/star_stats"
+# pattern <- "/home/btmonier/Temporary/sorghum_rna_stats/star_stats/"
+path <- "C:/Users/brand/Documents/tmp/sorghum_rna_statistics/star_stats"     # <- Windows path
+pattern <- "C:/Users/brand/Documents/tmp/sorghum_rna_statistics/star_stats/" # <- Windows path
+meta_path <- "C:/Users/brand/Documents/tmp/sorghum_rna_statistics/"          # <- Windows path
 
 
 ## Load data ----
@@ -42,6 +45,9 @@ files <- list.files(
 stats_ls <- lapply(X = seq_len(length(files)), FUN = function(i) {
     readLines(con = files[i])
 })
+
+### Read metadata
+metadata <- readr::read_csv(file = paste0(meta_path, "/", "sorghum_rna_metadata.csv"))
 
 
 
@@ -65,11 +71,12 @@ mapped_multiple_ls <- vector(mode = "numeric", length = length(stats_ls))
 mapped_too_many_ls <- vector(mode = "numeric", length = length(stats_ls))
 unmapped_short_ls  <- vector(mode = "numeric", length = length(stats_ls))
 unmapped_other_ls  <- vector(mode = "numeric", length = length(stats_ls))
+total_reads        <- vector(mode = "numeric", length = length(stats_ls))
 
 
 ## Loop ----
 for (i in seq_len(length(stats_ls))) {
-    
+
     message("Parsing sample: ", pure_names[i])
 
     # Subset for each taxa
@@ -114,6 +121,7 @@ for (i in seq_len(length(stats_ls))) {
 
     # Populate
     map_speed_ls[i]       <- tmp_map_speed
+    total_reads[i]        <- tmp_total
     uniquely_mapped_ls[i] <- tmp_uniq
     mapped_multiple_ls[i] <- tmp_map_mult
     mapped_too_many_ls[i] <- tmp_map_too
@@ -126,6 +134,7 @@ for (i in seq_len(length(stats_ls))) {
 star_data <- tibble::tibble(
     sample          = pure_names,
     map_speed       = map_speed_ls,
+    total_reads     = total_reads,
     uniquely_mapped = uniquely_mapped_ls,
     mapped_multiple = mapped_multiple_ls,
     mapped_too_many = mapped_too_many_ls,
@@ -134,12 +143,58 @@ star_data <- tibble::tibble(
 )
 
 
+## Filter ----
+
+### Sorghum panel
+star_data_clean <- star_data %>%
+    dplyr::filter(
+        !stringr::str_detect(sample, "B73|BLANK|BT623|BTx623|s_|20_")
+    ) %>%
+    dplyr::arrange(-total_reads)
+
+### Checks
+star_data_checks <- star_data %>%
+    dplyr::filter(
+        stringr::str_detect(sample, "B73|BLANK|BT623|BTx623|s_|20_")
+    ) %>%
+    dplyr::arrange(-total_reads)
+
+
+## Remove variables ----
+rm(
+    map_speed_ls,
+    total_reads,
+    uniquely_mapped_ls,
+    mapped_multiple_ls,
+    mapped_too_many_ls,
+    unmapped_short_ls,
+    unmapped_other_ls,
+    tmp_map_mult,
+    tmp_map_speed,
+    tmp_map_too,
+    tmp_total,
+    tmp_uniq,
+    tmp_unmap_other,
+    tmp_unmap_short
+)
+
+
+## Write data to disk ----
+write.csv(
+    x = star_data,
+    file = "data/sorghum_star_stats.csv",
+    row.names = FALSE
+)
+
+
 
 # === Visualize =====================================================
 
 ## Convert data to long format ----
-long_star <- star_data %>%
-    tidyr::gather(key = "map_type", value = "value", 3:7)
+star_data_clean$sample_id <- seq_len(nrow(star_data_clean)) %>%
+    as.numeric()
+long_star <- star_data_clean %>%
+    tidyr::gather(key = "map_type", value = "value", 4:8)
 
 
 ## Reorder factor levels ----
@@ -154,11 +209,12 @@ long_star$map_type <- factor(
     )
 )
 
-long_star %>%
+
+## Plot STAR alignment scores ----
+star_scores <- long_star %>%
     ggplot() +
-    aes(x = sample, y = value, fill = map_type) +
-    geom_col(position = position_stack(reverse = TRUE)) +
-    coord_flip() +
+    aes(x = sample_id, y = value, fill = map_type) +
+    geom_area(position = position_stack(reverse = TRUE)) +
     scale_fill_manual(
         values = c("#4579A6", "#77B4EE", "#E1AB76", "#BA0948", "#780505"),
         name = "",
@@ -177,6 +233,7 @@ long_star %>%
             "Unmapped: other"
         )
     ) +
+    xlab("Sorghum samples arranged by total input reads") +
     ylab("Number of reads") +
     ggtitle("STAR alignment scores") +
     expand_limits( x = c(0,NA), y = c(0,NA)) +
@@ -184,18 +241,18 @@ long_star %>%
         paste0(round(l / 1e6, 1), "M")
     }) +
     theme_minimal() +
-    theme(legend.position = "bottom", axis.title.y = element_blank())
+    theme(
+        legend.position = "bottom",
+        axis.text.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()
+    )
 
 
-# star_maps %>%
-#     ggplot() +
-#     aes(y = map_percent) +
-#     geom_boxplot() +
-#     ylab("% Uniquely mapped reads") +
-#     theme(
-#         axis.title.x = element_blank(),
-#         axis.text.x = element_blank(),
-#         axis.ticks.x = element_blank()
-#     )
+## Save plot to disk ----
+ggsave(
+    filename = "C:/Users/brand/Documents/tmp/sorghum_rna_statistics/star_scores.png",
+    plot = star_scores, device = "png",width = 10, height = 5, dpi = "retina"
+)
 
 
