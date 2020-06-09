7f1e6c6a83739af514ea830aeb435b5138b8a301
Brandon <brandon.monier@gmail.com>
2020-06-09 15:18:52 -0400

    Add visualizations; fix up NA bugs

--- R/weather_forecast.R
+++ R/weather_forecast.R
@@ -5,7 +5,7 @@
 # Description:   Scrape weather.gov for weather data
 # Author:        Brandon Monier
 # Created:       2020-06-07 at 15:34:32
-# Last Modified: 2020-06-07 at 17:32:09
+# Last Modified: 2020-06-09 at 15:18:21
 #--------------------------------------------------------------------
 
 #--------------------------------------------------------------------
@@ -17,8 +17,11 @@
 # === Preamble ======================================================
 
 ## Load packages ----
-library(dplyr)
+library(dplyr, quietly = TRUE)
+library(ggplot2)
+library(lubridate)
 library(magrittr)
+library(patchwork)
 library(rvest)
 library(tidyr)
 library(xml2)
@@ -40,6 +43,9 @@ con <- paste0(base, city_coord, data_type)
 
 
 ## Parameters ----
+
+### Time zone
+tz <- "America/New_York"
 
 ### Modified column names
 cols <- c(
@@ -104,12 +110,21 @@ df_wthr <- df_wthr %>%
 df_wthr <- df_wthr %>%
     tidyr::fill(date) %>%
     dplyr::mutate(
-        date = as.factor(date),
+        date = lubridate::as_datetime(
+            paste(date, hour),
+            format = "%m/%d %H",
+            tz = tz
+        )
+    ) %>%
+    dplyr::mutate(
         wind_dir = factor(
             wind_dir,
             levels = c("N", "NE", "E", "SE", "S", "SW", "W", "NW")
-        )
+        ),
+        rain = as.factor(rain),
+        thunder = as.factor(thunder)
     ) %>%
+    dplyr::select(-hour) %>%
     dplyr::mutate_if(is.character, as.numeric)
 
 
@@ -118,15 +133,92 @@ print(df_wthr)
 
 
 
+# === Visualize =====================================================
+
+## Temperature ----
+plt_temp <- df_wthr %>%
+    dplyr::select(date, temp_f, dewpoint_f, heat_index_f) %>%
+    tidyr::pivot_longer(c("temp_f", "dewpoint_f", "heat_index_f")) %>%
+    dplyr::mutate(
+        name = factor(
+            name,
+            levels = c("temp_f", "heat_index_f", "dewpoint_f"),
+            labels = c("Temperature", "Heat index", "Dew point")
+        )
+    ) %>%
+    ggplot() +
+    aes(x = date, y = value, color = name) +
+    geom_line(size = 1, na.rm = TRUE) +
+    geom_point(na.rm = TRUE) +
+    ylab("Temperature (F)") +
+    theme(
+        axis.title.x = element_blank(),
+        legend.title = element_blank(),
+        legend.position = "bottom"
+    )
 
 
+## Wind speed ----
+plt_wind <- df_wthr %>%
+    dplyr::select(date, surface_wind_mph, wind_dir, gust) %>%
+    tidyr::pivot_longer(c("surface_wind_mph", "gust")) %>%
+    dplyr::mutate(
+        name = factor(
+            name,
+            levels = c("surface_wind_mph", "gust"),
+            labels = c("Surface wind", "Gusts")
+        )
+    ) %>%
+    ggplot() +
+    aes(x = date, y = value, color = name) +
+    geom_line(size = 1, na.rm = TRUE) +
+    geom_point(na.rm = TRUE) +
+    ylab("Wind speed (mph)") +
+    theme(
+        axis.title.x = element_blank(),
+        legend.title = element_blank(),
+        legend.position = "bottom"
+    )
 
 
+## Percentages ----
+plt_perc <- df_wthr %>%
+    dplyr::select(date, sky_cover_perc, precip_potential_perc, relative_humidity_perc) %>%
+    tidyr::pivot_longer(c("sky_cover_perc", "precip_potential_perc", "relative_humidity_perc")) %>%
+    dplyr::mutate(
+        name = factor(
+            name,
+            levels = c("relative_humidity_perc", "precip_potential_perc", "sky_cover_perc"),
+            labels = c("Relative humidity", "Precipitation potential", "Sky cover")
+        )
+    ) %>%
+    ggplot() +
+    aes(x = date, y = value, color = name) +
+    geom_line(size = 1, na.rm = TRUE) +
+    geom_point(na.rm = TRUE) +
+    ylab("%") +
+    theme(
+        axis.title.x = element_blank(),
+        legend.title = element_blank(),
+        legend.position = "bottom"
+    )
 
 
+## Plot all ----
+plt_all <- (plt_temp) / (plt_wind) / (plt_perc)
 
+start_stop <- c(
+    df_wthr[[1, 1]],
+    df_wthr[[48, 1]]
+)
 
-
+plt_all <-  plt_all +
+    plot_annotation(
+        title = "Weather forecast",
+        subtitle = paste0(df_wthr[[1, 1]], " - ", df_wthr[[48, 1]]),
+        caption = "source: weather.gov"
+    )
+print(plt_all)
 
 
 
